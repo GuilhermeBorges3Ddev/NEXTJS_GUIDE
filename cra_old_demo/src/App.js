a24730e695cd3b25624f85f4ab67c787abb23402
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2023-12-16 19:57:17 -0300

    Basic form example with object population done in App.js

--- cra_old_demo/src/App.js
+++ cra_old_demo/src/App.js
@@ -8,9 +8,22 @@ const USER_NAMES = {
 
 function App() {
   const [user, setUser] = useState("Random man");
+  const [formName, setFormName] = useState("");
+  const [formAge, setFormAge] = useState(0);
+  const [formMail, setFormMail] = useState("");
+  const [fullFormData, setFullFormData] = useState({});
   function handleChangeName() {
     if (user === "Random man") setUser(USER_NAMES.firstUser);
     else setUser("Random man");
+  }
+  function handleRegister(e) {
+    e.preventDefault();
+    alert("User registered successfully");
+    setFullFormData({
+      formName,
+      formAge,
+      formMail,
+    });
   }
   return (
     <div>
@@ -22,6 +35,72 @@ function App() {
       <button style={{ cursor: "pointer" }} onClick={handleChangeName}>
         Change first user name
       </button>
+      <br />
+      <h1 style={{ margin: "20 0 0 10" }}>
+        First login? Register your infos first...
+      </h1>
+      <form style={{ margin: 20 }} onSubmit={handleRegister}>
+        <label>Name:</label>
+        <br />
+        <input
+          type="text"
+          minLength={1}
+          placeholder="Type your name"
+          value={formName}
+          onChange={(e) => setFormName(e.target.value)}
+        />
+        <br />
+        <label>Years:</label>
+        <br />
+        <input
+          type="number"
+          min={0}
+          placeholder="Type your age"
+          value={formAge}
+          onChange={(e) => setFormAge(e.target.value)}
+        />
+        <br />
+        <label>Email:</label>
+        <br />
+        <input
+          type="mail"
+          placeholder="Type your most used email"
+          value={formMail}
+          onChange={(e) => setFormMail(e.target.value)}
+        />
+        <br />
+        <button
+          style={{
+            cursor: "pointer",
+            display: "block",
+            marginTop: 10,
+            flexWrap: "nowrap",
+          }}
+          type="submit"
+        >
+          Register formulary
+        </button>
+      </form>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          border: "1px dashed red",
+          width: "300px",
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+        }}
+      >
+        <div style={{ width: "100%" }}>Form name: {fullFormData.formName}</div>
+        <br />
+        <div style={{ width: "100%" }}>
+          Form age: {fullFormData.formAge ? fullFormData.formAge : ""}
+        </div>
+        <br />
+        <div style={{ width: "100%" }}>Form mail: {fullFormData.formMail}</div>
+        <br />
+      </div>
     </div>
   );
 }
