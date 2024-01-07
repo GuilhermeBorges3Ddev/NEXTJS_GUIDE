990ce37fedcbe841dc9e43d2366e9b316e2c56f0
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2024-01-06 22:46:25 -0300

    Github API getter for projects done, mounted in starred-projects-lab, on the Main component

--- starred-projects-lab/src/pages/Main/index.jsx
+++ starred-projects-lab/src/pages/Main/index.jsx
@@ -1,5 +1,49 @@
-import { Title } from "./styles";
+import api from "../../services/api";
+import { useState, useCallback } from "react";
+import { FaGithub, FaPlus } from "react-icons/fa";
+import { Container, Form, SubmitButton } from "./styles";
 
 export default function Main() {
-  return <Title>Main page</Title>;
+  const [newRepo, setNewRepo] = useState("");
+  const [repositories, setRepositories] = useState([]);
+
+  function handleInputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      async function triggerGithubGetRepo() {
+        const response = await api.get(`repos/${newRepo}`);
+        const data = {
+          name: response.data.full_name,
+        };
+        setRepositories([...repositories, data]);
+        setNewRepo("");
+      }
+      triggerGithubGetRepo();
+    },
+    [newRepo, repositories]
+  );
+
+  return (
+    <Container>
+      <h1>
+        <FaGithub />
+        My repositories:
+      </h1>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newRepo}
+          onChange={handleInputChange}
+          placeholder="Add your gisted repos..."
+        />
+        <SubmitButton>
+          <FaPlus color="white" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
